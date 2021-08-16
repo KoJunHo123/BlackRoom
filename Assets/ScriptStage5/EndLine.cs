5a6ab84cba37b53dccf1a88c88897dc479d3ee39
KoJunHo123 <88295444+KoJunHo123@users.noreply.github.com>
2021-08-16 16:16:38 +0900

    Stage

---
+++ Assets/ScriptStage5/EndLine.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndLine : MonoBehaviour
+{
+    StageManager5 stage5;
+
+    void Start()
+    {
+        stage5 = FindObjectOfType<StageManager5>();
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Answer")
+        {
+            Destroy(other.gameObject);
+            stage5.GameOver = true;
+        }
+        if(other.gameObject.tag == "Wrong")
+        {
+            Destroy(other.gameObject);
+        }
+    }
+
+}
--- Assets/ScriptStage5/Sword.cs
+++ Assets/ScriptStage5/Sword.cs
@@ -2,25 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using UnityEngine.UI;
+
 public class Sword : MonoBehaviour
 {
-    StageManager5 Manager;
-    // Start is called before the first frame update
+    public LayerMask AnswerLayer;
+    public LayerMask WrongLayer;
+    StageManager5 stage5;
+
     void Start()
     {
-        
+        // var gameObj = GameObject.FindWithTag("Score");
+        // score = gameObj.GetComponent<Score>();
+        stage5 = FindObjectOfType<StageManager5>();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
-    }
-    private void OnTriggerEnter(Collider other)
-    {
-        Manager = FindObjectOfType<StageManager5>();
-        if (other.gameObject.tag == "Answer")
-            Destroy(other);
-        else Manager.GameOver = true;
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, transform.forward, out hit, 1, AnswerLayer))
+        {
+            Destroy(hit.transform.gameObject);
+        }
+        else if(Physics.Raycast(transform.position, transform.forward, out hit, 1, WrongLayer))
+        {
+            Destroy(hit.transform.gameObject);
+            stage5.GameOver = true;
+        }
     }
 }
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    Manager = FindObjectOfType<StageManager5>();
+    //    if (other.gameObject.tag == "Answer")
+    //        Destroy(other);
+    //    else Manager.GameOver = true;
+    //}
