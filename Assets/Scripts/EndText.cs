4f6e9c842f01e4b37659c814f0898bd0033e5561
KoJunHo123 <88295444+KoJunHo123@users.noreply.github.com>
2021-08-19 15:12:37 +0900

    GameEnd아마도 최종

--- Assets/Scripts/EndText.cs
+++ Assets/Scripts/EndText.cs
@@ -7,15 +7,27 @@ public class EndText : MonoBehaviour
     public GameObject TextEnd;
     public GameObject TextTryAgain;
 
+    public GameObject YesTryAgain;
+    public GameObject NoTryAgain;
+    
+    public GameObject YesEnd;
+    public GameObject NoEnd;
+
     private void Awake()
     {
+        PlayerPrefs.SetInt("NewStage", 1);
+
         if(PlayerPrefs.GetInt("NewStage") == 0)
         {
             TextEnd.SetActive(true);
+            YesEnd.SetActive(true);
+            NoEnd.SetActive(true);
         }
         else
         {
             TextTryAgain.SetActive(true);
+            YesTryAgain.SetActive(true);
+            NoTryAgain.SetActive(true);
         }
     }
     void Start()
