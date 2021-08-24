ff7a2ed9b475358545d533c7b46c4271ad26e07f
KoJunHo123 <88295444+KoJunHo123@users.noreply.github.com>
2021-08-24 15:47:09 +0900

    GameOver3 끝

--- Assets/Scripts/HittingSound.cs
+++ Assets/Scripts/HittingSound.cs
@@ -5,13 +5,18 @@ using UnityEngine;
 public class HittingSound : MonoBehaviour
 {
     [SerializeField] AudioSource hitAudioPlayer;
+
+
     public ParticleSystem DustEffect;
     public float timer1;
     public float timer2;
+    public float timer3;
     public float hittingTime;
+    public float BoomTime;
     public float hitTerm;
     float i;
 
+    public Transform hitArea;
 
     void Start()
     {
@@ -23,16 +28,24 @@ public class HittingSound : MonoBehaviour
     {
         timer1 += Time.deltaTime;
         timer2 += Time.deltaTime;
+        timer3 += Time.deltaTime;
+
         if (timer1 > hittingTime)
         {
             if (timer2 > hitTerm)
             {
                 hitAudioPlayer.Play();
                 timer2 = 0;
-                DustEffect.Play();
-                
             }
-
+            
+        }
+        if(timer1 > BoomTime)
+        {
+            if (timer3 > hitTerm)
+            {
+                Instantiate(DustEffect, hitArea.position, Quaternion.Euler(0, 0, 0));
+                timer3 = 0;
+            }
         }
 
 
