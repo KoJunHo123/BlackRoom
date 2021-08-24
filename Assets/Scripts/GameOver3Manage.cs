f98ac466214cbd21e918b32d02456973040da0f4
LCH991115 <88644795+LCH991115@users.noreply.github.com>
2021-08-24 09:21:32 +0900

    3

---
+++ Assets/Scripts/GameOver3Manage.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver3Manage : MonoBehaviour
+{
+    [SerializeField] GameObject FadeIn;
+    [SerializeField] GameObject FadeOut;
+    float timer;
+    public float UsingTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        FadeIn.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > 10.0f)
+        {
+            FadeOut.SetActive(true);
+
+            if (timer > UsingTime)
+                SceneManager.LoadScene("GameEnd");
+        }
+    }
+}
